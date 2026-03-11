ea32d22d63c0137f109b8030ca21a96236a1ab91
Joel Dic <joeldic@Joels-MacBook-Air.local>
2026-03-11 18:45:42 -0400

    Fixed buttons

--- functions/index.js
+++ functions/index.js
@@ -10,10 +10,17 @@ exports.grantTime = onRequest({ secrets: [sshKey] }, (req, res) => {
   
   const conn = new Client();
   conn.on("ready", () => {
-    // If status, run --userinfo. If add, run -t <minutes>
-    const cmd = action === 'status' 
-      ? `timekpra --userinfo ${kid}` 
-      : `timekpra -t ${minutes} --user ${kid}`;
+    // If status, run --userinfo. If add/subtract, run --settimeleft with appropriate operator. If reset, set to 0.
+    let cmd;
+    if (action === 'status') {
+      cmd = `timekpra --userinfo ${kid}`;
+    } else if (action === 'add') {
+      cmd = `timekpra --settimeleft ${kid} '+' ${minutes * 60}`;
+    } else if (action === 'subtract') {
+      cmd = `timekpra --settimeleft ${kid} '-' ${minutes * 60}`;
+    } else if (action === 'reset') {
+      cmd = `timekpra --settimeleft ${kid} '=' 0`;
+    }
 
     conn.exec(cmd, (err, stream) => {
       let data = '';
@@ -23,15 +30,27 @@ exports.grantTime = onRequest({ secrets: [sshKey] }, (req, res) => {
         res.status(200).send(data); // Send back the output of timekpra
       });
     });
-  }).connect({ /* ... your config ... */ });
+  }).on("error", (err) => {
+    res.status(500).send("Connection failed: " + err.message);
+  }).connect({
+    host: "209.227.149.77",
+    port: 50022,
+    username: "sshuser",
+    privateKey: sshKey.value(),
+    readyTimeout: 10000
+  });
 });
 
 exports.testConnection = onRequest({ secrets: [sshKey] }, (req, res) => {
   const conn = new Client();
   conn.on("ready", () => {
     conn.exec("hostname", (err, stream) => {
-      stream.on('close', () => { conn.end(); });
-      res.status(200).send("Connection successful! Server hostname: " + stream.stdout);
+      let data = '';
+      stream.on('data', (d) => { data += d; });
+      stream.on('close', () => { 
+        conn.end(); 
+        res.status(200).send("Connection successful! Server hostname: " + data);
+      });
     });
   }).on("error", (err) => {
     res.status(500).send("Connection failed: " + err.message);
